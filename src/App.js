dc3f65b84d6e802d30341853b58857e2ba3773a3
vasearar <prodius345@gmail.com>
2024-09-03 16:16:10 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
+
 const tg = window.Telegram.WebApp;
 
 function App() {
-  useEffect(()=> {
-    tg.ready();
-  }, [])
+  const [count, setCount] = useState(0);
+  const [scale, setScale] = useState(1);
 
-  const onClose = () => {
-    tg.close()
-  }
+  const handleCatClick = () => {
+    setCount(count + 1);
+    setScale(0.8);
 
+    setTimeout(() => {
+      setScale(1);
+    }, 300);
+  };
 
   return (
     <div className="App">
-      Vaflist
-      <button onClick={onClose}>Close</button>
+      <h2 className="sus">We are open!</h2>
+      <div className="center">
+        <img
+          alt="cat"
+          src="img1.jpg"
+          className="cat"
+          style={{ transform: `scale(${scale})`, transition: 'transform 0.3s ease' }}
+          onClick={handleCatClick}
+        />
+      </div>
+      <h3>{count}</h3>
+      <div className='center margin-top'>
+       <button>Get real</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/Clicker.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Clicker = () => {
+  return (
+    <div>Coin</div>
+  )
+}
+
+export default Clicker
